01dc6638f4b505e6f79c1f6f976abbcc221e0fef
albert-bikeev <albert.bikeev@gmail.com>
2024-10-17 16:25:32 +0200

    Docs link

--- segmentation/PixelLib.py
+++ segmentation/PixelLib.py
@@ -2,13 +2,15 @@ import pixellib
 from pixellib.torchbackend.instance import instanceSegmentation
 
 ins = instanceSegmentation()
-ins.load_model("pointrend_resnet50.pkl")  ## Это нужно скачать
+## Это нужно скачать отсюда https://github.com/ayoolaolafenwa/PixelLib/blob/master/Tutorials/Pytorch_image_instance_segmentation.md#:~:text=Download%20the%20PointRend%20model.
+ins.load_model("pointrend_resnet50.pkl")
 target_classes = ins.select_target_classes(dog=True)
 # save_extracted_objects=True - сохраняет вырезанные объекты в папках в input_folder почему-то,
 # но можно то же самое из res или out вытащить похоже
 res, out = ins.segmentBatch(input_folder="data/filtered_dom_lapkin_2_small", segment_target_classes=target_classes,
                             show_bboxes=True, extract_segmented_objects=True,
-                            save_extracted_objects=True, output_folder_name="data/filtered_dom_lapkin_2_small_segmented",
+                            save_extracted_objects=True,
+                            output_folder_name="data/filtered_dom_lapkin_2_small_segmented",
                             text_size=1.1)
 print('\n\n\n\n\n------------------------------')
 print('res:')
