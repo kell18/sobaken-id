12d35bf216b4d90230a071fde851803f704929b5
albert-bikeev <albert.bikeev@gmail.com>
2024-11-03 17:38:22 +0100

    Add WanDB and use it for train-reid.py

--- re-identification/train-reid.py
+++ re-identification/train-reid.py
@@ -1,64 +1,100 @@
 import torch
 import torchreid
-from dom_lapkin import DomLapkin13
-import glob
-import os
-from torchreid.data.datasets import ImageDataset
+from torchreid.engine import ImageTripletEngine, ImageSoftmaxEngine
+from my_datasets.vk_posts_3 import VkPosts3
+import wandb
 
+training_model_version = '0.2_vkp3_only2_or_more_photos_test'
 
 if __name__ == '__main__':
+    run = wandb.init(project="sobaken-id", name=f'train_reid_v{training_model_version}',
+                     settings=wandb.Settings(_disable_stats=True, _disable_meta=True))
 
-    torchreid.data.register_image_dataset('DomLapkin13', DomLapkin13)
+    dataset = VkPosts3
+    save_dir = f'log/v{training_model_version}'
+
+    config = run.config
+    config.base_pretrained_model = 'osnet_x1_0'
+    config.dataset_name = dataset.name
+
+    config.transforms = ['random_flip', 'random_crop', 'random_erase']
+
+    config.optimizer = 'adam'
+    config.loss = 'triplet'
+    config.learning_rate = 3e-4
+
+    config.scheduler_method = 'single_step'
+    config.scheduler_stepsize = 20
+
+    config.max_epochs = 60
+
+    print(f'train_model_version: {training_model_version}, configuration: {config}')
+
+    torchreid.data.register_image_dataset(dataset.name, dataset)
 
     # Create the data manager
     datamanager = torchreid.data.ImageDataManager(
-        root='/Users/albert.bikeev/Projects/sobaken-id/data/clean',
-        sources='DomLapkin13',
+        root=dataset.base_path,
+        sources=dataset.name,
         height=256,
         width=128,
         batch_size_train=32,
         batch_size_test=100,
-        transforms=['random_flip', 'random_crop', 'random_erase'],
+        transforms=config.transforms,
         num_instances=4,
         train_sampler='RandomIdentitySampler',
     )
 
+    print(f"Number of training identities: {datamanager.num_train_pids}")
+
     # Build the model
     model = torchreid.models.build_model(
-        name='osnet_x1_0',
+        name=config.base_pretrained_model,
         num_classes=datamanager.num_train_pids,
-        loss='triplet',
+        loss=config.loss,
         pretrained=True
     )
+
     # Move the model to GPU if available
     model = model.cuda() if torch.cuda.is_available() else model
+
+    # Print model's classifier output dimension
+    print(f"Model's classifier output dimension: {model.classifier.out_features}")
 
     # Build optimizer and scheduler
     optimizer = torchreid.optim.build_optimizer(
         model,
-        optim='adam',
-        lr=3e-4
+        optim=config.optimizer,
+        lr=config.learning_rate,
     )
     scheduler = torchreid.optim.build_lr_scheduler(
         optimizer,
-        lr_scheduler='single_step',
-        stepsize=20
+        lr_scheduler=config.scheduler_method,
+        stepsize=config.scheduler_stepsize
     )
 
+    engine_class = ImageTripletEngine if config.loss == 'triplet' else ImageSoftmaxEngine
+
     # Build engine
-    engine = torchreid.engine.ImageSoftmaxEngine(
+    engine = engine_class(
         datamanager,
         model,
         optimizer=optimizer,
         scheduler=scheduler,
-        label_smooth=True
+        label_smooth=True,
+        log_metrics=lambda metrics: run.log(metrics),
     )
 
-    # Run training
-    engine.run(
-        save_dir='log/v0.1.12e_lTrip-dl1-3',
-        max_epoch=12,
-        eval_freq=3,
-        print_freq=3,
-        test_only=False
-    )
+    try:
+        # Run training
+        engine.run(
+            save_dir=save_dir,
+            max_epoch=config.max_epochs,
+            eval_freq=3,
+            print_freq=10,
+            test_only=False
+        )
+    finally:
+        print('Training was finished or interrupted, trying to send WanDB metrics...')
+        run.finish()
+        print('WanDB metrics are sent.')
